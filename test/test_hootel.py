9c437db58a30dfffb9175c81bc2fd3e2a3eefd27
ForroEva <142794017+ForroEva@users.noreply.github.com>
2024-01-14 11:51:05 +0100

    homework test_hootel.py

--- test/test_hootel.py
+++ test/test_hootel.py
@@ -57,4 +57,47 @@ class TestHootel(object):
         hotel_list = self.browser.find_elements(By.XPATH, '//h4[@style="cursor: pointer"]')
         assert len(hotel_list) != 0
         assert len(hotel_list) == 10
-        
+
+    def test_hotel_checkbox(self):
+        checkbox_list = self.browser.find_elements(By.XPATH, '//input[@type="checkbox"]')
+        for checkbox in checkbox_list:
+            checkbox.click()
+            assert checkbox.is_selected()
+
+        erase_selection_btn = self.browser.find_element(By.ID, 'redstar')
+        erase_selection_btn.click()
+
+        for checkbox in checkbox_list:
+            check_class = checkbox.get_attribute('class')
+            if 'ng-pristine' in check_class:
+                print('A checkbox nincs kijelölve.')
+
+    def test_chose_hotel (self):
+        first_hotel = self.browser.find_element(By.XPATH, '//h4[@style="cursor: pointer"]')
+            first_hotel.click()
+            time.sleep(1)
+
+    def test_hotel_descriptin(self):
+        hotel_description = self.browser.find_elements(By.XPATH, '//p[@class="card-text"]')[1]
+        print(hotel_description.text)
+        assert 500 <= len(hotel_description.text) <= 2000
+
+    def test_booking(self):
+        booking_btn = self.browser.find_element(By.ID, 'user-bookings')
+        booking_btn.click()
+        time.sleep(2)
+
+        erase_actual_booking_btn = self.browser.find_elements(By.XPATH, '//div[@id="actual"]//button')
+        erase_past_booking_btn = self.browser.find_elements(By.XPATH, '//div[@id="past"]//button')
+
+        if len(erase_actual_booking_btn) >= 1:
+            print(f'A felhasználónak van jelenlegi foglalása: {len(erase_actual_booking_btn)} db.')
+        else:
+            print('A felhasználónak nincs jelenlegi foglalása.')
+
+        if len(erase_past_booking_btn) >= 1:
+            print(f'A felhasználónak van múltbeli foglalása: {len(erase_past_booking_btn)} db.')
+        else:
+            print('A felhasználónak nincs múltbeli foglalása.')
+
+        browser.quit()
